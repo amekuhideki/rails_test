aec882d9d1f2589218a2585747480ff6a740bdcd
amekuhideki <h_ameku@funteam.co.jp>
2017-09-06 23:54:12 +0900

    Use strong parameters for user create action

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -15,4 +15,11 @@ class UsersController < ApplicationController
   else
     render 'new'
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                             :password_confirmation)
+  end
 end
