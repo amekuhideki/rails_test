12a2d24b7877007d117c04d0f2e13b056746bdd8
amekuhideki <h_ameku@funteam.co.jp>
2017-09-16 15:27:55 +0900

    Fix user registration

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -9,17 +9,17 @@ class UsersController < ApplicationController
   end
 
   def create
-  @user = User.new(params[:user])    # 実装は終わっていないことに注意!
-  if @user.save
+    @user = User.new(user_params)
+    if @user.save
     # 保存の成功をここで扱う。
-  else
-    render 'new'
+    else
+      render 'new'
+    end
   end
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                             :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
