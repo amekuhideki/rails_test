505595acdf6a30ada77f11227a2d0b7d5d8a1e6e
amekuhideki <h_ameku@funteam.co.jp>
2017-09-26 23:53:23 +0900

    Fix test

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -13,6 +13,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       end
       assert_template 'users/new'
       assert_select 'div#error_explanation'
-      assert_select 'div.alert'   
+      assert_select 'div.alert'
+      assert_select 'form[action="/signup"]'
     end
 end
